2fd4ca2c16983e2b22a8c72fe591c1b1c4231f3f
jbnilles <jbnilles24@gmail.com>
2020-09-29 14:59:17 -0700

    add initial files and folders
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>
    Co-Authored-By: Natalie Murphy <Nataliemurphy500@gmail.com>

---
+++ JobBoard/Controllers/JobOpeningsController.cs
@@ -0,0 +1,31 @@
+using JobBoard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace JobBoard.Controllers
+{
+  public class JobOpeningsController : Controller
+  {
+    [HttpGet("/JobOpenings")]
+    public ActionResult Index()
+    {
+      List<JobOpening> jobs = JobOpening.GetAll();
+      return View(jobs);
+    }
+
+    [HttpGet("/JobOpenings/Form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+
+    [HttpPost("/JobOpenings")]
+    public ActionResult Create(string title,string description, string name, string email, string phoneNumber)
+    {
+      Contact contact = new Contact(name, email, phoneNumber);
+      JobOpening job = new JobOpening(title, description, contact); 
+      
+      return RedirectToAction("Index");
+    }
+  }
+}
---
+++ JobBoard/README.md
@@ -0,0 +1,10 @@
+## Job Board
+### Natalie Murphy - Janet Karpenske - Joseph Niles
+#### Sept 29, 2020 
+Create a section of a job posting board where a user can create one instance of a JobOpening class by submitting a form. It should include properties for a title, description, and contact info for the person posting the job.
+
+If you're feeling fancy, make the contact info into an object with its own class declaration. Hint: A Contact object would include properties like a name, email and phoneNumber, and you would want to store it inside the JobOpening object.
+
+## Technology
+c# 
+.Net Core 
